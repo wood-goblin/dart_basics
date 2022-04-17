faa11949e850f495c8e640f8c0b2a050dc7fa2f8
alex <alex@MacBook-Pro-Alex.mail>
2022-04-17 21:54:51 +0500

    добавил метод конвертации из двоичной системы в десятичную

--- bin/integer_converter.dart
+++ bin/integer_converter.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
+
 class IntegerConverter {
-  int num;
+  IntegerConverter();
 
-  IntegerConverter(this.num);
-
-  String toBinary() {
+  // конвертация из десятичной системы в двоичную
+  String toBinary(int num) {
     var binaryStr = [];
     while (num > 0) {
       binaryStr.add(num % 2);
@@ -11,10 +12,29 @@ class IntegerConverter {
     }
     return binaryStr.reversed.join();
   }
+
+  // конвертация из двоичной системы в десятичную
+  int toInt1(String binaryStr) {
+    int num = 0; // десятичный результат
+    var i = 1; // incremental binary degree (2, 4, 8, 16, 32, ...)
+    for (String s in binaryStr.split('').reversed) {
+      num += int.parse(s) * i;
+      i *= 2;
+    }
+    return num;
+  }
 }
 
 void main() {
-  print("ЗАДАЧА 2. Целое в двоичное");
-  print("17 -> " + IntegerConverter(17).toBinary());
-  print("10 -> " + IntegerConverter(10).toBinary());
+  print("ЗАДАЧА 2.1: Целое в двоичное");
+  print("17 -> " + IntegerConverter().toBinary(17));
+  print("10 -> " + IntegerConverter().toBinary(10));
+  print("13 -> " + IntegerConverter().toBinary(13));
+  print('');
+
+  print("ЗАДАЧА 2.2: Двоичное в целое");
+  // 1 * 2^0 + 0 * 2^1 + 1 * 2^2 + 1 * 2^3 = 1 + 0 + 4 + 8 = 13
+  print("10001 -> ${IntegerConverter().toInt1('10001')}");
+  print("1010 -> ${IntegerConverter().toInt1('1010')}");
+  print("1101 -> ${IntegerConverter().toInt1('1101')}");
 }
